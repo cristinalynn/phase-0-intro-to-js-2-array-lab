ca0052421e4499937e54643080c89446bb9a5488
Cristina Lynn <cristina@cristinalynn.net>
2022-12-11 06:29:13 -0800

    commit tree 85d51aa6176a82fee0882d26a04a06a2d773f0df

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+ function destructivelyAppendCat(name) {
+cats.push(name);
+ }
+ function destructivelyPrependCat(name) {
+    cats.unshift(name);
+ }
+ function destructivelyRemoveLastCat() {
+    cats.pop();
+ }
+ function destructivelyRemoveFirstCat() {
+    cats.shift();
+ }
+ function appendCat(name) {
+     const catsArray = [ ...cats, name];
+     return(catsArray);
+ }
+ function prependCat(name) {
+    const catsArray = [name, ...cats];
+    return(catsArray);
+ }
+ function removeLastCat() {
+    const catsArray = cats.slice(0, -1);
+    return(catsArray);
+ }
+function removeFirstCat() {
+    const catsArray = cats.slice(1);
+    return(catsArray);
+}
+  
