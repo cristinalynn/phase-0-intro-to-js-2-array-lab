e7663d61f08ad8d67f60d69d61680892c0bc5253
Cristina Lynn <cristina@cristinalynn.net>
2023-01-17 11:05:17 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+  function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+  }
+  function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+  }
+  function appendCat(name) {
+    const catsArray = [ ...cats, name];
+    return(catsArray);
+  }
+  function prependCat(name) {
+    const catsArray = [name, ...cats];
+    return(catsArray);
+  }
+  function removeLastCat(name) {
+    const catsArray = cats.slice(0, -1);
+    return(catsArray);
+  }
+  function removeFirstCat(name) {
+    const catsArray = cats.slice(1);
+    return(catsArray);
+  }
+
